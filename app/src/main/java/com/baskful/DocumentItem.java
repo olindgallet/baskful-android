e0da586cc902fee48853bad1358d6250400602b4
Olin Gallet <olindgallet@gmail.com>
2022-12-25 21:52:17 -0600

    Refactored frags to accurate names, has item names

--- app/src/main/java/com/baskful/DocumentItem.java
+++ app/src/main/java/com/baskful/DocumentItem.java
@@ -9,11 +9,11 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
-import com.baskful.databinding.FragmentFirstBinding;
+import com.baskful.databinding.DocumentItemBinding;
 
-public class FirstFragment extends Fragment {
+public class DocumentItem extends Fragment {
 
-    private FragmentFirstBinding binding;
+    private DocumentItemBinding binding;
 
     @Override
     public View onCreateView(
@@ -21,7 +21,7 @@ public class FirstFragment extends Fragment {
             Bundle savedInstanceState
     ) {
 
-        binding = FragmentFirstBinding.inflate(inflater, container, false);
+        binding = DocumentItemBinding.inflate(inflater, container, false);
         return binding.getRoot();
 
     }
